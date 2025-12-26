c51b1dfe64b99893901d8c42091a589ad185c972
Teng Xing Tan <TANT441323@exactsoftware.com>
2025-12-26 17:24:38 +0800

    Add channel: init, store fetched data in channel, then read from channel

--- main.go
+++ main.go
@@ -18,26 +18,36 @@ func main() {
 	start := time.Now()
 
 	var wg sync.WaitGroup
+
+	channel := make(chan int)
  	
 	postIDs := []int{1,2,3,4,5,10,50,75,100}
 	
 	for _, id := range postIDs {
 		wg.Add(1)
-		go fetchPost(id, &wg)
+		go fetchPost(id, &wg, channel)
 	}
-	
-	// Block the main function here until the counter returns to 0
-	wg.Wait()
+
+	// Start a goroutine to Wait and Close the channel.
+	go func() {
+		// Block the main function here until the counter returns to 0
+		wg.Wait() 
+		close(channel)
+	}()
+
+	for id := range channel {
+    	fmt.Println("Received:", id)
+	}
 
 	fmt.Printf("\nDone processed %d requests in %v\n", len(postIDs), time.Since(start))
 }
 
-func fetchPost(id int, wg *sync.WaitGroup) {
+func fetchPost(id int, wg *sync.WaitGroup, channel chan int) {
 	defer wg.Done()
 
 	url := fmt.Sprintf("https://jsonplaceholder.typicode.com/posts/%d", id)
 
-	res, err := http.Get(url)	
+	res, err := http.Get(url)
 
 	if err != nil {
 		log.Printf("Error fetching post %d: %v", id, err)
@@ -50,6 +60,8 @@ func fetchPost(id int, wg *sync.WaitGroup) {
 		log.Printf("Error decoding post %d: %v", id, err)
 		return
 	}
+
+	channel <- post.ID
 
 	fmt.Printf("Successfully fetched Post #%d, %s\n", post.ID, post.Title)
 }
